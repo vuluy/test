8b244ce0c13ab08ddcc14a27f685abe727a8f747
vuluy <vuluy@vuluy-deMacBook-Air.local>
2017-03-24 19:47:38 +0800

    test-branch2
    !!!

---
+++ HelloWorld-branch2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : yoyo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
